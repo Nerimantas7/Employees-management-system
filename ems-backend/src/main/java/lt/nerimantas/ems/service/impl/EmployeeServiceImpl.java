031eec23cb88111e9e65e9e6b06c97596bb8f901
Nerimantas7 <nerimantas.tarabilda@stud.techin.lt>
2024-06-11 10:42:00 +0300

    Added files and updated till 53 lecture

--- ems-backend/src/main/java/lt/nerimantas/ems/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/lt/nerimantas/ems/service/impl/EmployeeServiceImpl.java
@@ -42,4 +42,25 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeedId, EmployeeDto updatedEmployee) {
+
+        Employee employee = employeeRepository.findById(employeedId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee is not exist with given id: " + employeedId));
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee is not exist with given id: " + employeeId));
+        employeeRepository.deleteById(employeeId);
+    }
 }
